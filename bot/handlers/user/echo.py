dbfb38c3fe4f2b2c778d8db23835bc9c4e3e2be8
cbekoder <cbekoder@gmail.com>
2024-08-13 13:44:05 +0500

    fix: deleted unneeded codes

--- bot/handlers/user/echo.py
+++ bot/handlers/user/echo.py
@@ -11,7 +11,6 @@ async def echo_handler(message: Message) -> None:
     try:
         # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
-        print(message)
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
--- bot/handlers/user/start.py
+++ bot/handlers/user/start.py
@@ -1,7 +1,6 @@
 from aiogram.filters import CommandStart
 from aiogram.types import Message, InlineKeyboardMarkup
 from aiogram import html
-from keyboards.inline.location_keyboards import send_location
 
 from loader import dp
 
@@ -10,4 +9,4 @@ async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    await message.answer(f"Salom, {html.bold(message.from_user.full_name)}!\nMa'lumot olish uchun joylashuv tanlang: ", reply_markup=send_location)
+    await message.answer(f"Salom, {html.bold(message.from_user.full_name)}!")
