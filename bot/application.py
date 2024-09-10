29fda9fa238b3d8cf6d8641fff92430cd4108e2a
cbekoder <cbekoder@gmail.com>
2024-09-11 02:23:29 +0500

    fixing applications.py

--- bot/application.py
+++ bot/application.py
@@ -4,17 +4,15 @@ import sys
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 from loader import bot, dp
+import handlers
 
 async def on_startup():
     await on_startup_notify()
     await set_default_commands()
 
-
 async def main() -> None:
     dp.startup.register(on_startup)
     await dp.start_polling(bot)
-
-
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
